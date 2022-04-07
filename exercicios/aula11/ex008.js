ed88473102f67bb6537195ac2a83e3af9d3b752a
Yan Di Nápoli Araújo <dinapoliyan@gmail.com>
2022-04-07 09:50:15 -0300

    Estrutura condicional simples

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel =  40
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 50)
+    console.log('Você ultrapassou a velocidade permitida. MULTADO')
+else
+    console.log('Dirija sempre usando sinto de segurança!')
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é brasileriro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
+    
